63fa302eb98765e9eb77a3de5925805764847fb8
Abu Isa Biswas <bmabuisa.cse@gmail.com>
2025-05-23 15:31:48 +0600

    initial setup complete

---
+++ src/main/java/com/isa/PatientRegistration/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.isa.PatientRegistration.repository;
+
+import com.isa.PatientRegistration.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
